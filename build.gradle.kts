9ba74ac3d8b96354201b60903adb1f8d45934c06
Eugen Druzin <edruzin@gmail.com>
2020-04-01 13:39:33 +0200

    Update dependecies
    AB#97

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,21 +7,21 @@ import com.google.protobuf.gradle.protoc
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "1.3.61"
+    val kotlinVersion = "1.3.71"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
 
-    id("org.springframework.boot") version "2.2.4.RELEASE"
+    id("org.springframework.boot") version "2.2.6.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("com.google.cloud.tools.jib") version "2.1.0"
     id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
     id("com.google.protobuf") version "0.8.11"
-    id("io.gitlab.arturbosch.detekt").version("1.6.0")
+    id("io.gitlab.arturbosch.detekt").version("1.7.2")
     idea
 }
 
 group = "com.ampnet"
-version = "0.1.8"
+version = "0.1.9"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
