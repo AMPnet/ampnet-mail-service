a20a872b04c4939d4bb53c1c3333c4a5d22d0007
Eugen Druzin <edruzin@gmail.com>
2020-10-12 16:00:33 +0200

    Add grpc security

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 }
 
 group = "com.ampnet"
-version = "0.3.0"
+version = "0.4.0"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
--- src/main/kotlin/com/ampnet/mailservice/grpc/GrpcInterceptorConfig.kt
+++ src/main/kotlin/com/ampnet/mailservice/grpc/GrpcInterceptorConfig.kt
@@ -1,11 +1,18 @@
 package com.ampnet.mailservice.grpc
 
 import net.devh.boot.grpc.client.interceptor.GlobalClientInterceptorConfigurer
+import net.devh.boot.grpc.server.security.authentication.GrpcAuthenticationReader
+import net.devh.boot.grpc.server.security.authentication.SSLContextGrpcAuthenticationReader
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class GrpcInterceptorConfig {
+
+    @Bean
+    fun authenticationReader(): GrpcAuthenticationReader {
+        return SSLContextGrpcAuthenticationReader()
+    }
 
     @Bean
     fun globalInterceptorConfigurerAdapter(): GlobalClientInterceptorConfigurer {
